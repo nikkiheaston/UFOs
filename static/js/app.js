ba4f5f4fec7e9fa0f337d19aa064d867f4b36fe3
nikkiheaston <nikkiheaston@gmail.com>
2022-06-02 17:39:17 -0700

    table built

--- static/js/app.js
+++ static/js/app.js
@@ -18,3 +18,15 @@ function buildTable(data) {
     });
 }
 
+function handleClick() {
+    let date = d3.select("#datetime").property("value");
+    let filteredData = tableData;
+    if (date) {
+        filteredData = filteredData.filter(row => row.datetime === date);
+    };
+    buildTable(filteredData);
+}
+
+d3.selectAll("filter-btn").on("click", handleClick);
+
+buildTable(tableData);
