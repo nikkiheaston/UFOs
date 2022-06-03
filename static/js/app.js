d3a6dc0eb091593146a24b765e67b332a7b06024
nikkiheaston <nikkiheaston@gmail.com>
2022-06-02 17:08:59 -0700

    created table

---
+++ static/js/app.js
@@ -0,0 +1,20 @@
+// import the data from data.js
+const tableData = data;
+
+// Reference the HTML table using d3
+var tbody = d3.select("tbody");
+
+// function to build data table
+function buildTable(data) {
+    tbody.html("");
+
+    data.forEach((dataRow) => {
+        let row = tbody.append("tr");
+        Object.values(dataRow).forEach((val) => {
+          let cell = row.append("td");
+          cell.text(val);
+          }
+        );
+    });
+}
+
